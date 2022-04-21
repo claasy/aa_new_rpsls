5f5318acdd9766244fabb076019381034937a4e1
Amanda <aclaas522@gmail.com>
2022-04-21 14:48:50 -0500

    Initialized RPSLS Project and Created Classes

---
+++ New RPSLS/New RPSLS/PlayerClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace New_RPSLS;
+
+public abstract class PlayerClass
+{
+	public string name;
+	public string gestures 'rock','paper','scissors','lizard','spock';
+	public int score;
+
+
+	
+	public PlayerClass()
+	{
+	}
+}
---
+++ New RPSLS/PlayerClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace New_RPSLS
+
+public abstract class PlayerClass
+{
+	public string name;
+	
+	public PlayerClass()
+	{
+	}
+}
